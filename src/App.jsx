fb9ee8ecf5a0264e84e7cdcdc8d27c60063d408e
Minhaj Islam <minhajislam674@gmail.com>
2023-04-03 11:32:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import NewsletterForm from "./components/NewsletterForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <NewsletterForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NewsletterForm.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const NewsletterForm = () => {
+  //We pass our form’s initialValues and a submission function (onSubmit) to the useFormik() hook.
+  //The hook then returns to us a goodie bag of form state and helper methods in a variable we call formik.
+
+  // handleSubmit: A submission handler
+  // handleChange: A change handler to pass to each <input>, <select>, or <textarea>
+  // values: Our form’s current values
+
+  return (
+    <div className="form-container">
+      <Formik
+        initialValues={{
+          firstName: "",
+          email: "",
+        }}
+        validationSchema={Yup.object({
+          firstName: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Required"),
+          email: Yup.string()
+            .email("Invalid email address")
+            .required("Required"),
+        })}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form className="form-components">
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+          <ErrorMessage
+            className="error-message"
+            component="p"
+            name="firstName"
+          />
+
+          <label htmlFor="email">Email Address</label>
+          <Field name="email" type="email" />
+          <ErrorMessage component="p" className="error-message" name="email" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default NewsletterForm;
